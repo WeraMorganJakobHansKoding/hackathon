215c5cf91517a2b3099de8a755cc94bb0abd83a0
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 15:47:25 -0600

    Correcting stuff for country data

--- dataProcess.php
+++ dataProcess.php
@@ -15,7 +15,7 @@ foreach ($data_container as $theme_name => $theme_properties) {
     echo "<pre>";
     var_dump ($theme_properties->column_names);
     echo "</pre>";
-    foreach ($theme_properties['column_names'] as $column_titles_key => $column_titles_value) {
+    foreach ($theme_properties->column_names as $column_titles_key => $column_titles_value) {
         if ($column_titles_value === 'Value') {
             $theme_value_key = $column_titles_key;
         }
@@ -23,7 +23,7 @@ foreach ($data_container as $theme_name => $theme_properties) {
             $theme_date_key = $column_titles_key;
         }
     }
-    foreach ($theme_properties['data'] as $data_property) {
+    foreach ($theme_properties->data as $data_property) {
         $date_value = strtotime($data_property[$theme_date_key]);
         $year_value = date('Y', $date_value);
         if ($data_property[$theme_value_key] === 0) {

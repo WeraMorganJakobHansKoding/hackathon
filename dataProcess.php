979c9810fc4ed27eac2c698a41c10c3b2079d0e7
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 07:40:11 -0600

    Testing error reporting and PHP parsing

--- dataProcess.php
+++ dataProcess.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('error_reporting', 'E_ALL & ~E_NOTICE');
+ini_set('error_reporting', 'E_ALL & E_NOTICE');
 
 require('dataClass.php');
 
@@ -9,3 +9,5 @@ $new_call = new Data();
 var_dump($new_call->testCall());
 
 ?>
+
+HOLA!

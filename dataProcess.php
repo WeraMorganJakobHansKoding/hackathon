8ccf44e28acbf664c60032f6ab2a91c8375b638d
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 16:43:21 -0600

    Finished data processing and ready for displaying

--- dataProcess.php
+++ dataProcess.php
@@ -24,15 +24,55 @@ foreach ($data_container as $theme_name => $theme_properties) {
         $date_value = strtotime($data_property[$theme_date_key]);
         $year_value = date('Y', $date_value);
         if ($data_property[$theme_value_key] < 0.00000001) {
-            $country_data[$theme_name][$year_value] = FALSE;
+            $country_data[$theme_name][$year_value] = NULL;
         } else if (2010 >= intval($year_value)) {
             $country_data[$theme_name][$year_value] = $data_property[$theme_value_key];
         }
     }
 }
 
+foreach ($country_data['military'] as $military_year => $military_value) {
+    if (isset($country_data['exchange_usd'][$military_year])) {
+        $country_data['military'][$military_year] = $military_value * $country_data['exchange_usd'][$military_year];
+    } else {
+        $country_data['military'][$military_year] = NULL;
+    }
+}
+
+foreach ($country_data['gni'] as $gni_year => $gni_value) {
+    if (isset($country_data['exchange_usd'][$gni_year])) {
+        $country_data['gni'][$gni_year] = $gni_value * $country_data['exchange_usd'][$gni_year];
+    } else {
+        $country_data['gni'][$gni_year] = NULL;
+    }
+}
+
+foreach ($country_data['education'] as $education_year => $education_value) {
+    if (isset($country_data['gni'][$education_year])) {
+        $country_data['education'][$education_year] = $education_value * $country_data['gni'][$education_year];
+    } else {
+        $country_data['education'][$education_year] = NULL;
+    }
+}
+
+foreach ($country_data['population'] as $population_year => $population_value) {
+    $year_int = intval($population_year);
+    $previous_year = (int) $year_int - 1;
+    if (isset($country_data['population'][$previous_year])) {
+        $country_data['population_growth'][$population_year] = $population_value - $country_data['population'][$previous_year];
+    } else {
+        $country_data['population_growth'][$population_year] = NULL;
+    }
+}
+
+foreach ($country_data as $country_theme_name => $country_theme_data) {
+    foreach ($country_theme_data as $country_theme_year => $country_theme_year_value) {
+        $finished_data[$country_theme_year][$country_theme_name] = $country_theme_year_value;
+    }
+}
+
 echo "<pre>";
-var_dump($country_data);
+var_dump($finished_data);
 echo "</pre>";
 
 ?>

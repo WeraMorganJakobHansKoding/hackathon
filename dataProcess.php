5a2ce96a6d156d7c66f8c7fb0acc15e55c8f0d19
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 11:49:42 -0600

    Calling all countries at once

--- dataProcess.php
+++ dataProcess.php
@@ -2,9 +2,11 @@
 
 require('dataClass.php');
 
-echo "<pre>";
 $data_object = new Data();
-var_dump($data_object->getCountryData('Colombia'));
-echo "</pre>";
-
+foreach ($data_object->countries as $country => $codes) {
+    echo "<h1>" . $country . "</h1>";
+    echo "<pre>";
+    var_dump($data_object->getCountryData($country));
+    echo "</pre>";
+}
 ?>

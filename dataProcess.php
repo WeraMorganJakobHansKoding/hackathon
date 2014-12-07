8bdcb653b609279244a6d504f72aab3c054b7d37
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 11:39:28 -0600

    Testing Chile

--- dataProcess.php
+++ dataProcess.php
@@ -4,7 +4,7 @@ require('dataClass.php');
 
 echo "<pre>";
 $data_object = new Data();
-var_dump($data_object->getCountryData('Canada'));
+var_dump($data_object->getCountryData('Chile'));
 echo "</pre>";
 
 ?>

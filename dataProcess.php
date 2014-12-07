c73092a6db290b7cd3624c55d41144982aed2fef
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 11:27:14 -0600

    Testing Canada

--- dataProcess.php
+++ dataProcess.php
@@ -4,7 +4,7 @@ require('dataClass.php');
 
 echo "<pre>";
 $data_object = new Data();
-var_dump($data_object->getCountryData('Brasil'));
+var_dump($data_object->getCountryData('Canada'));
 echo "</pre>";
 
 ?>

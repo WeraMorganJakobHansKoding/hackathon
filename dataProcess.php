8b6b9f790e8b619b1a5a4ccc391879ad5755ff16
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 18:52:04 -0600

    st

--- dataProcess.php
+++ dataProcess.php
@@ -81,8 +81,9 @@ unset($finished_data['1994']);
 
 
 $content_template = new Smarty();
-/*
+
 $content_template->assign('data', $finished_data);
+/*
 $content_html = $content_template->fetch('tiles.tpl');
  */
 echo $finished_data;

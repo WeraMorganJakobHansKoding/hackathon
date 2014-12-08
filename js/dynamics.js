0d86dd3248c9eeeb5020cd81b07dc0bbb39e1152
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-08 00:49:15 -0600

    Second test for country title

--- js/dynamics.js
+++ js/dynamics.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
             $('body').loadingOverlay('remove');
             $('#tilesWrapper').empty().append(data);
             $('.year' + $('#slider').slider("value")).slideDown();
-            $('#countryTitle').text($(this).text());
+            $('h3#countryTitle').text($(this).text());
         });
 
         return false;

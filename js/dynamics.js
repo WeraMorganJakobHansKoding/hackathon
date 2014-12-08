3fbb4338afd5a7c6e53e1a366f8530ab3fe4f51e
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 23:06:35 -0600

    Fixed minor problem

--- js/dynamics.js
+++ js/dynamics.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('#slider').slider({
         min: 1995,
         max: 2010,
-        slide; function(event, ui) {
+        slide: function(event, ui) {
             $('.currentTimes').html(ui.value);
         },
         change: function(event, ui) {

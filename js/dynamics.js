660c28a83a5cfacdb4e3177ff2d8496207a7e617
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 22:50:18 -0600

    Testing slider

--- js/dynamics.js
+++ js/dynamics.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     $('#slider').slider({
         min: 1995,
-        max: 2010
+        max: 2010,
+        change: function(event, ui) {
+            alert(ui.value);
+        }
     });
 });

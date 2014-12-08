4229270e53365a031c667834b70c3714d2a9ff50
Jakob H. Renpening <jakobhans@gmail.com>
2014-12-07 23:05:22 -0600

    Messing with the slider

--- js/dynamics.js
+++ js/dynamics.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $('#slider').slider({
         min: 1995,
         max: 2010,
+        slide; function(event, ui) {
+            $('.currentTimes').html(ui.value);
+        },
         change: function(event, ui) {
             alert(ui.value);
         }
